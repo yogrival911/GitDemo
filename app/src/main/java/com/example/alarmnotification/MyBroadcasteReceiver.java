84865a6550ebfd1c7ad00e7cb7b072c626728fe9
YOGESH <yogrival911@gmail.com>
2020-11-19 09:48:33 +0530

    Broadcaste

---
+++ app/src/main/java/com/example/alarmnotification/MyBroadcasteReceiver.java
@@ -0,0 +1,19 @@
+package com.example.alarmnotification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadcasteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        Toast.makeText(context, "Broadcaste Received", Toast.LENGTH_SHORT).show();
+        Log.i("yog", "Alarm Broadcaste Triggered");
+
+    }
+}
